cffcb4c661b33de99c02876de0356faeaa0cde63
GibberishRacoon <GibberishRacoon@users.noreply.github.com>
2024-01-08 22:21:31 +0100

    eee

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "jsx-a11y"],
+  rules: {},
+};
